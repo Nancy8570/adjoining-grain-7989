a857c6ef0f03951a408a11b2945916a1648bce6a
Nancy8570 <rananancy91@gmail.com>
2023-03-05 20:13:47 +0530

    done

--- course_monitoring_system/src/com/masai/dao/Facultydao.java
+++
@@ -1,9 +0,0 @@
-package com.masai.dao;
-
-public interface Facultydao {
-public boolean  loginFaculty();
-	
-	public String viewCoursePlan();
-	public String dayWisePlanner();
-	public String updatePassowrd();
-}
